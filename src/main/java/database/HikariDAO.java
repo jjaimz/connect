c41eb558209447bdce3ea7b630b8c85f331a2767
Shubhkarman Pruthi <likeaboss10211@gmail.com>
2024-06-25 16:56:18 -0400

    Added HikariCP to connect with "connect" database

---
+++ src/main/java/database/HikariDAO.java
@@ -0,0 +1,28 @@
+package database;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+
+public class HikariDAO {
+    private static HikariConfig config = new HikariConfig();
+    private static HikariDataSource ds;
+
+    static {
+        config.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        config.setJdbcUrl( "jdbc:mysql://localhost:3306/connect");
+        config.setUsername( "shubh" );
+        config.setPassword( "password" );
+        config.addDataSourceProperty( "cachePrepStmts" , "true" );
+        config.addDataSourceProperty( "prepStmtCacheSize" , "250" );
+        config.addDataSourceProperty( "prepStmtCacheSqlLimit" , "2048" );
+        ds = new HikariDataSource( config );
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return ds.getConnection();
+    }
+}
